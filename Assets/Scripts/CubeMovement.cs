ffddb8c64a5afd405d3bae09ff3f7e575f55b568
Helioz <mjduque.19@est.ucab.edu.ve>
2023-05-09 19:56:16 -0400

    Movement Physics complete and background design

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -8,6 +8,8 @@ public class CubeMovement : MonoBehaviour
  //Creacion de variables para detectar que boton se esta pulsando ambas por defecto seran falsas
   bool Left = false;
   bool Right = false;
+  private Animator Animator;
+
 
   public Rigidbody2D rb;
   public float MovementSpeed;
@@ -15,38 +17,51 @@ public class CubeMovement : MonoBehaviour
   public void clickLeft(){
      
      Left = true;
+     Animator.SetBool("running", true);
+    
 
   }
 
   public void releaseLeft(){
 
       Left=false;
+      Animator.SetBool("running", false);
 
   }
 
   public void clickRight(){
 
     Right=true;
+    Animator.SetBool("running", true);
 
   }
 
   public void releaseRight(){
 
     Right=false;
+    Animator.SetBool("running", false);
 
   }
 
   public void FixedUpdate(){
 
+     Animator = GetComponent<Animator>();
+
     if (Left)
      {
       rb.AddForce(new Vector2(-MovementSpeed,0 )* Time.deltaTime);
+      transform.localScale = new Vector3(-1.0f,1.0f,1.0f);
+      
      }
 
      if (Right)
      {
       rb.AddForce(new Vector2(MovementSpeed,0 )* Time.deltaTime);
+      transform.localScale = new Vector3(1.0f,1.0f,1.0f);
+      
      }
+
+
      
   }
  
