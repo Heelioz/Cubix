1f3c8fe3e2daba4497e8668b2eb5cb467e943924
Helioz <mjduque.19@est.ucab.edu.ve>
2023-05-09 21:16:06 -0400

    Main menu

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,23 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+   //Este script funciona para hacer la transicion de la escena principal 
+
+   public void Play(){
+
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+
+   }
+
+   public void Salir(){
+
+    Debug.Log("Salir---");
+   
+    Application.Quit();
+    }
+}
