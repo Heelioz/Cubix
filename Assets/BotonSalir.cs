9a816f468a8c95b1d856b655a25279ddd961a1ab
Helioz <mjduque.19@est.ucab.edu.ve>
2023-05-09 21:32:50 -0400

    Little updates (Exit Button)

---
+++ Assets/BotonSalir.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonSalir : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public void Salir(){
+
+    Debug.Log("Salir---");
+   
+    Application.Quit();
+    }
+
+}
